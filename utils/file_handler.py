127f7d6364f3363743c2b40d558c9abcc4631768
Jonatas Onca <jonatas.onca@hotmail.com>
2025-10-03 20:44:47 -0400

    Melhorias

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,12 +1,51 @@
 import json
 import yaml
 
+from jinja2 import Template
 from apache_beam.io.filesystems import FileSystems
 
 def load_yaml(bucket_yaml: str) -> str:
     """Lê um arquivo .yaml do GCS."""
-    with FileSystems.open(bucket_yaml) as f:
-        return yaml.safe_load(f)
+    # --- Etapa 1: Ler o arquivo como texto bruto ---
+    try:
+        with FileSystems.open(bucket_yaml) as file:
+            # Lê todo o conteúdo do arquivo para uma string
+            # 1. file.read() retorna um objeto de bytes
+            yaml_as_bytes = file.read()
+
+            # 2. Precisamos decodificar os bytes para uma string (usando utf-8)
+            # para que o Jinja2 e o PyYAML possam processá-la.
+            yaml_as_text = yaml_as_bytes.decode('utf-8')
+
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{bucket_yaml}' não foi encontrado.")
+        exit()
+
+    # --- Etapa 2: Carregar o YAML uma vez para obter as variáveis ---
+    # Usamos safe_load para segurança
+    try:
+        # Este dicionário conterá o bloco 'variables' que precisamos
+        initial_data = yaml.safe_load(yaml_as_text)
+    except yaml.YAMLError as e:
+        print(f"Erro ao analisar o YAML inicial: {e}")
+        exit()
+        
+    try:
+        # --- Etapa 3: Renderizar o texto usando Jinja2 ---
+        # Criamos um objeto de template Jinja2 a partir do nosso texto
+        template = Template(yaml_as_text)
+
+        # Renderizamos o template, passando os dados que carregamos
+        # para que o Jinja saiba o que substituir.
+        rendered_yaml_text = template.render(initial_data)
+    except yaml.YAMLError as e:
+        print(f"Erro ao Renderizar o texto usando Jinja2: {e}")
+        exit()
+    # --- Etapa 4: Carregar o YAML final (já processado) ---
+    # Agora que os placeholders {{ }} foram substituídos,
+    # podemos carregar o YAML como um dicionário Python limpo.
+    return yaml.safe_load(rendered_yaml_text)
+
     
 def load_query(bucket_query: str) -> str:
     """Lê um arquivo .sql do GCS."""
