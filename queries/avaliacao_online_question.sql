deae5905884f326c09ce67f5355d8a8c2d456600
Jonatas Onca <jonatas.onca@hotmail.com>
2025-10-06 04:43:07 -0400

    Correcao

--- queries/avaliacao_online_question.sql
+++ queries/avaliacao_online_question.sql
@@ -4,7 +4,7 @@ SELECT
     CAST(createdAt AS CHAR) AS createdAt,
     CAST(description AS CHAR) AS description,
     CAST(id AS SIGNED) AS id,
-    CAST(order AS SIGNED) AS order,
+    CAST(`order` AS SIGNED) AS `order`,
     CAST(pageId AS SIGNED) AS pageId,
     CAST(questionTemplateId AS SIGNED) AS questionTemplateId,
     CAST(updatedAt AS CHAR) AS updatedAt
